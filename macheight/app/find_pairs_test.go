4041efd1ca76fa60bc4dcef12795f9567a46eda3
Richard Almanza <al.vegari@gmail.com>
2023-05-16 15:42:14 -0500

    add benchmark

--- macheight/app/find_pairs_test.go
+++ macheight/app/find_pairs_test.go
@@ -6,13 +6,25 @@ import (
 )
 
 func Test(t *testing.T) {
-	for _, tC := range findPairsTestCases {
-		t.Run(tC.description, func(t *testing.T) {
-			actual := FindPairIntegers(tC.numbers, tC.sum)
+	for _, tc := range findPairsTestCases {
+		t.Run(tc.description, func(t *testing.T) {
+			actual := FindPairIntegers(tc.numbers, tc.sum)
 
-			if !reflect.DeepEqual(actual, tC.expected) {
-				t.Fatalf("FindPairIntegers(%v, %v) = %v, want: %v", tC.numbers, tC.sum, actual, tC.expected)
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Fatalf("FindPairIntegers(%v, %v) = %v, want: %v", tc.numbers, tc.sum, actual, tc.expected)
 			}
 		})
+	}
+}
+
+func Benchmark(b *testing.B) {
+	if testing.Short() {
+		b.Skip("skipping benchmark in short mode.")
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, tc := range findPairsTestCases {
+			FindPairIntegers(tc.numbers, tc.sum)
+		}
 	}
 }
